d1f391dee213e8d1381d5f6de4f66e7d128ca475
Oliver Reyes <oliver.dentistry@gmail.com>
2020-08-30 20:24:20 +0200

    Updates

---
+++ precache-manifest.d1ca4e7a3c7228ea0320fc9690174fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f62ee8c581f06627d1d8d45fe34eb39",
+    "url": "/guerreroz01/index.html"
+  },
+  {
+    "revision": "70c509f19e47f08de8ba",
+    "url": "/guerreroz01/static/css/main.6c7c84d0.chunk.css"
+  },
+  {
+    "revision": "5ff5c6018e28beda8139",
+    "url": "/guerreroz01/static/js/2.ab82fc8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/guerreroz01/static/js/2.ab82fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c509f19e47f08de8ba",
+    "url": "/guerreroz01/static/js/main.b39bd320.chunk.js"
+  },
+  {
+    "revision": "1f29c20d274c22f553b9",
+    "url": "/guerreroz01/static/js/runtime-main.e5b6959c.js"
+  }
+]);
